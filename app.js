ca8461b424955e6a88d505599758bd3e1062175e
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-17 14:49:34 -0700

    promisyfing alterfile

--- app.js
+++ app.js
@@ -3,24 +3,37 @@
 const fs = require('fs');
 
 const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
+  readFile(file)
+    .then(data => {
+      upper(data);
+    })
+    .then(text => {
+      console.log(text);
+      writeFile(text);
     });
-  });
 };
 
 function readFile(file){
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
       if(err) { reject(err); }
-      resolve(data.toString().toUpperCase());
+      resolve(data.toString());
 
     });
   });
+}
+function upper(data){
+  return new Promise(resolve => {
+    resolve(data.toUpperCase());
+  });
+}
+function writeFile(file){
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, Buffer.from(text), (err, data) => {
+      if(err) { reject(err); }
+      console.log(`${file} saved`);
+    })
+  })
 }
 
 let file = process.argv.slice(2).shift();
